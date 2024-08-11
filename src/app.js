2360e8d90e889b36524b9d8af39aa81fa0b9edf8
Kushal1825 <kushalkalsariya@gmail.com>
2024-08-11 20:20:48 +0530

    error handling and response

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parse'
+
+const app = express()
+
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true,
+}))
+
+app.use(express.json({limit:'100kb'})) // data send by json file 
+
+app.use(express.urlencoded({extended:true,limit:"16kb"})) // data send by url middleware
+app.use(express.static("public"))
+app.use(cookieParser()) //cookie fetch or set middleware using cookie parser
+
+export { app }
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,7 @@
+const assyncHandler = (requestHandler)=>{
+    return (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next)).catch((err)=> next(err))
+    }
+}
+
+export {assyncHandler}
